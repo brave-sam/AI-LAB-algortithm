10389b2d65a5c4be1cf16422df4f1abe426d3aae
Sam <103090422+brave-sam@users.noreply.github.com>
2023-09-14 22:27:55 +0530

    Added Table Output

--- bfs_graph.py
+++ bfs_graph.py
@@ -1,46 +1,38 @@
 from tabulate import tabulate
 
+def bfs(graph):
+    table = []
+    headers = ["Current Node", "Queue", "Processed Nodes", "Status"]
+    visited = {node: 1 for node in graph}
+    start_node = next(iter(graph))
+    queue = [start_node]
+    visited[start_node] = 2
+    processed_nodes = []
+    
+    while queue:
+        current_node = queue.pop(0)
+        processed_nodes.append(current_node)
+        
+        for neighbor in graph[current_node]:
+            if visited[neighbor] == 1:
+                queue.append(neighbor)
+                visited[neighbor] = 2
+        
+        visited[current_node] = 3
+        table.append([current_node, " ".join(queue), " ".join(processed_nodes), visited.copy()])
+    
+    print(tabulate(table, headers=headers, tablefmt="grid"))
 
-_graph = {
-  "A":['F','C','B'],
-  "B":['A','C','B'],
-  "C":['A','B','D','E','F','G'],
-  "D":['C','F','E','J'],
-  "E":['C','D','G','J','K'],
-  "F":['A','C','D'],
-  "G":['B','C','E','K'],
-  "J":['D','E','K'],
-  "K":['E','G','J']
+graph = {
+    "a": ["f", "c", "b"],
+    "b": ["a", "c", "g"],
+    "c": ["a", "b", "d", "e", "f", "g"],
+    "d": ["c", "f", "e", "j"],
+    "e": ["c", "d", "g", "j", "k"],
+    "f": ["a", "c", "d"],
+    "g": ["b", "c", "e", "k"],
+    "j": ["d", "c", "j"],
+    "k": ["e", "g", "j"],
 }
-'''TO DO list 
-   Current Node 
-   , Queue 
-   , Processoed Nodes Status Matrix 
-   '''
 
-_visited=[]
-_queue=[]
-
-
-def bfs(_visited,_graph,node):
-    print("Current Nodes")
-    _visited.append(node)
-    _queue.append(node)
-    
-    while _queue:
-        S = _queue.pop(0)
-      
-        print(S)
-
-        for _adjacent in _graph[S]:
-            if _adjacent not in _visited:
-                _visited.append(_adjacent)
-                _queue.append(_adjacent)
-                
-                print(_queue)
-                
-                print(_visited)
-                
-
-bfs(_visited,_graph,'A')
-    
+bfs(graph)
